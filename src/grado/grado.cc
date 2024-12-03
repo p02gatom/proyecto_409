9ca69a8b0cf890f843b64be1000109052c72c362
Manuel Gavilán Torres <p02gatom@uco.es>
2024-12-03 12:59:56 +0100

    Clase grado hecha

---
+++ src/grado/grado.cc
@@ -0,0 +1,12 @@
+#include <string>
+#include "grado.h"
+
+Grado::Grado(int codigo, int creditos, std::string nombre, std::string facultad)
+{
+
+    codigo_ = codigo;
+    creditos_ = creditos;
+    nombre_ = nombre;
+    facultad_ = facultad;
+
+}
---
+++ src/grado/grado.h
@@ -0,0 +1,38 @@
+#ifndef GRADO_H
+#define GRADO_H
+
+#include <string>
+
+class Grado
+{
+
+    private:
+
+        int codigo_, creditos_;
+        std::string nombre_, facultad_;
+
+    public:
+
+        Grado
+        (
+
+            int codigo,
+            int creditos,
+            std::string nombre,
+            std::string facultad
+
+        );
+
+    inline int getCodigo() {return codigo_;}
+    inline int getCreditos() {return creditos_;}
+    inline std::string getNombre() {return nombre_;}
+    inline std::string getFacultad() {return facultad_;}
+
+    inline void setCodigo(int codigo) {codigo_ = codigo;}
+    inline void setCreditos(int creditos) {creditos_ = creditos;}
+    inline void setNombre(std::string nombre) {nombre_ = nombre;}
+    inline void setFacultad(std::string facultad) {facultad_ = facultad;}
+
+};
+
+#endif
